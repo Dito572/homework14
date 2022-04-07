843954518d2ec8680926df3494257305b320be31
Dito572 <83370275+Dito572@users.noreply.github.com>
2022-04-07 16:27:04 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+//1
+let myArr = [1, 2, 3, 4, 5];
+console.log(15 / myArr.length)
+//2
+let students = ["student1", "student2","student3","student4","student5"]
+let student1 = {
+    name:'luka',
+    age:21
+}
+let student2={
+    name:'vano',
+    age:19
+}
+let student3={
+    name:'gvanca',
+    age:19
+}
+let student4={
+    name:'giorgi',
+    age:20
+}
+let student5={
+    name:'gocha',
+    age:22
+}
+    
+//3
+let myInfo = {
+    firstName:'dito',
+    lastName:'mezurnishvili',
+    address: ['tbilisi', 'georgia'],
+    age: 19,
+    phoneNumbers: 1122334455
+};
+//4
+console.log('My name is ' + myInfo['firstName'] + ' My age is ' + myInfo['age'] + ' My address is ' + myInfo.address[0] );
